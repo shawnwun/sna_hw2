2ef44f4c20b322adc9e209a9525137467a76c99e
Allen <chenglun@yahoo.com>
2011-10-18 02:47:21 +0800

    Allen: sir model done

---
+++ src/sir_model.py
@@ -0,0 +1,116 @@
+import sys;
+import networkx as nx;
+import random;
+from networkx import *;
+
+#0 = susceptible
+#1 = infected
+#2 = recovered
+
+BIRTH = 0.5;
+DEATH = 0.5;
+
+def main(argv=None):
+    inputFile = sys.argv[1];
+    infectFile = sys.argv[2];
+
+    sirGraph = readGraph(inputFile);
+    [sirGraph, infected] = addInfectNodes(infectFile, sirGraph);
+
+   
+    iCount = 0;
+
+    
+    while (True):
+        newInfect = [];
+        newRecover = [];
+
+        s = 0;
+        i = 0;
+        r = 0;
+
+        #finding next infected nodes
+        for infectN in infected:            
+            neighborList = sirGraph.neighbors(infectN);            
+            
+            for n, attr in sirGraph.nodes_iter(True):
+                if n in neighborList:
+                    st = attr['status'];
+
+                    if (st == 0):
+                        rand = random.random();
+                
+                        if (rand < BIRTH):
+                            newInfect.append(n);
+
+            
+            rand = random.random();
+            if (rand < DEATH):                            
+                newRecover.append(infectN);
+
+
+        for n in newInfect:
+            sirGraph.add_node(n, status=1);
+
+        for n in newRecover:
+            sirGraph.add_node(n, status=2);
+
+        infected = [];
+        for n, attr in sirGraph.nodes_iter(True):
+            
+            if (attr['status'] == 0):
+                s+=1;                
+            if (attr['status'] == 1):
+                i+=1;
+                infected.append(n);
+            if (attr['status'] == 2):
+                r+=1;
+        
+        print "Iter: %d, new infect: %d, new recover: %d, s: %d, i: %d, r:%d" %(iCount, len(newInfect), len(newRecover), s, i, r);
+        iCount+=1;
+
+
+        if (len(newInfect) == 0 and len(newRecover) == 0):
+            break;
+
+
+def addInfectNodes(infectFilePath, g):
+    inputFh = open(infectFilePath, 'r');
+    infected = []
+    
+    for line in inputFh:
+        infectedNode = line.replace("\n", "");
+
+        if infectedNode in g:            
+            g.add_node(infectedNode, status = 1);
+            infected.append(infectedNode);
+            #g[infectedNode]['status'] = 1;
+        
+    inputFh.close();
+
+    return [g, infected];
+
+def readGraph(filePath):
+    inputFh = open(filePath, 'r');
+
+    diGraph = nx.DiGraph();
+
+    for line in inputFh:
+        line = line.replace("\n", "");
+        splitLine = line.partition("\t");
+
+        if (len(splitLine) == 3):
+            node1 = splitLine[0];
+            node2 = splitLine[2];
+
+            
+            diGraph.add_node(node1, status = 0);
+            diGraph.add_node(node2, status = 0);
+            diGraph.add_edge(node1, node2);        
+
+    inputFh.close();
+
+    return diGraph;
+
+if __name__ == "__main__":
+    sys.exit(main());
