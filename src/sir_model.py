4c853effb5f929b34c8bfa30503b6cbd91885ca8
Allen <chenglun@yahoo.com>
2011-10-18 03:05:40 +0800

    ay: add comments

--- src/sir_model.py
+++ src/sir_model.py
@@ -11,6 +11,11 @@ BIRTH = 0.5;
 DEATH = 0.5;
 
 def main(argv=None):
+
+    if len(sys.argv) < 3:
+        print "ERROR!! Usage: python sir_model.py cleanGraphPath infectedPath";
+        exit();
+    
     inputFile = sys.argv[1];
     infectFile = sys.argv[2];
 
@@ -20,7 +25,6 @@ def main(argv=None):
    
     iCount = 0;
 
-    
     while (True):
         newInfect = [];
         newRecover = [];
@@ -30,34 +34,37 @@ def main(argv=None):
         r = 0;
 
         #finding next infected nodes
-        for infectN in infected:            
+        for infectN in infected:
+            #find the neighbors of infected nodes
             neighborList = sirGraph.neighbors(infectN);            
             
             for n, attr in sirGraph.nodes_iter(True):
                 if n in neighborList:
                     st = attr['status'];
 
+                    #if node status is susceptible, try to infect
                     if (st == 0):
                         rand = random.random();
                 
                         if (rand < BIRTH):
                             newInfect.append(n);
 
-            
+
+            #try to recover
             rand = random.random();
             if (rand < DEATH):                            
                 newRecover.append(infectN);
 
-
+        #update status in graph
         for n in newInfect:
             sirGraph.add_node(n, status=1);
 
         for n in newRecover:
             sirGraph.add_node(n, status=2);
 
+        #update infected list
         infected = [];
-        for n, attr in sirGraph.nodes_iter(True):
-            
+        for n, attr in sirGraph.nodes_iter(True):            
             if (attr['status'] == 0):
                 s+=1;                
             if (attr['status'] == 1):
@@ -65,12 +72,12 @@ def main(argv=None):
                 infected.append(n);
             if (attr['status'] == 2):
                 r+=1;
-        
+        #print sirGraph.nodes(True);
         print "Iter: %d, new infect: %d, new recover: %d, s: %d, i: %d, r:%d" %(iCount, len(newInfect), len(newRecover), s, i, r);
         iCount+=1;
 
 
-        if (len(newInfect) == 0 and len(newRecover) == 0):
+        if (i == 0):
             break;
 
 
